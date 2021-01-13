4437f3f38a390b04619965a4c0ecbfb8845fc4ee
zh58815519 <zhipu2007817d@126.com>
2021-01-13 09:33:39 +0800

    2

--- app/model.py
+++ app/model.py
@@ -12,7 +12,8 @@ class HospitalClass(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     # date = db.Column(db.String(128))
-    cid = db.Column(db.Integer, db.ForeignKey('hospitalclass.id'))
+    cid = db.Column(db.Integer, db.FornignKey('hospitalclass.id'))
+
     #时间跟着医生走，需要一张类似于选课，医生与科室的视图（工作时间）。防止时间冲突
 
 class PatientInfo(db.Model):
@@ -34,19 +35,19 @@ class DoctorInfo(db.Model):
 class DoctorCycle(db.Model):
     __tablename__ = 'doctorcycle'
     id = db.Column(db.Integer, primary_key=True)
-    doctorid = db.Column(db.String(64), db.ForeignKey('doctorinfo.id'))
-    classid = db.Column(db.String(64), db.ForeignKey('hospitalclass.id'))
+    doctorid = db.Column(db.String(64), db.FornignKey('doctorinfo.id'))
+    classid = db.Column(db.String(64), db.FornignKey('hospitalclass.id'))
 
 class OutPatientTimetable(db.Model):
     __tablename__ = 'outpatienttimetable'
     id = db.Column(db.Integer, primary_key=True)
-    doctorcycleid = db.Column(db.Integer, db.ForeignKey('doctorcycle.id'))
+    doctorcycleid = db.Column(db.Integer, db.FornignKey('doctorcycle.id'))
     date = db.Column(db.String(128)) #准备一个时间段与字符串数字相对应 01 -> 9:00 - 10： 00
     
-class InPatientTimetable(db.Model):
+class inPatientTimetable(db.Model):
     __tablename__ = 'inpatienttimetable'
     id = db.Column(db.Integer, primary_key=True)
-    doctorinfoid = db.Column(db.String(64), db.ForeignKey('doctorinfo.id'))
+    doctorinfoid = db.Column(db.String(64), db.FornignKey('doctorinfo.id'))
     date = db.Column(db.String(128))
 
 #缺少急诊
@@ -54,49 +55,45 @@ class InPatientTimetable(db.Model):
 class ExpertsTimetable(db.Model):
     __tablename__ = 'expertstimetable'
     id = db.Column(db.Integer, primary_key=True)
-    doctorinfoid = db.Column(db.String(64), db.ForeignKey('doctorinfo.id'))
-    cid = db.Column(db.Integer, db.ForeignKey('hospitalclass.id'))
+    doctorinfoid = db.Column(db.String(64), db.FornignKey('doctorinfo.id'))
+    cid = db.Column(db.Integer, db.FornignKey('hospitalclass.id'))
     date = db.Column(db.String(128))
 
 class Medicine(db.Model):
     __tablename__ = 'medicine'
-    id = db.Column(db.Integer, primary_key=True) #6位，1开头，自增
-    medicineclass = db.Column(db.Integer) #药品类别， 0代表中药， 1代表西药
+    id = db.Column(db.Integer, primary_key=True) #6位 1开头 自增
     medicinename = db.Column(db.String(128))
+    medicineprice = db.Column(db.Integer)
+    medicineclass = db.Column(db.Integer)        #1-中药 0-西药
 
-class CheckClass(db.Model):
+class Checkclass(db.Model):
     __tablename__ = 'checkclass'
     id = db.Column(db.Integer, primary_key=True)
-    checkcname = db.Column(db.String(64))
+    checkname = db.Column(db.String(64))
 
 class CheckItem(db.Model):
     __tablename__ = 'checkitem'
-    id = db.Column(db.Integer, primary_key=True) #6位，2开头，自增
+    id = db.Column(db.Integer, primary_key=True) #6位 2开头 自增
     checkitemname = db.Column(db.String(64))
-    itemclass = db.Column(db.Integer, db.ForeignKey('checkclass.id'))
+    blongclass = db.Column(db.Integer, db.FornignKey('checkclass.id'))
 
-class ExamClass(db.Model):
-    __tablename__ = 'examclass'
-    id = db.Column(db.Integer, primary_key=True) #6位，3开头，自增
-    examname = db.Column(db.String(64))
-
-class ExamItem(db.Model):
-    __tablename__ = 'examitem'
+class Testclass(db.Model):
+    __tablename__ = 'testclass'
     id = db.Column(db.Integer, primary_key=True)
-    examitemname = db.Column(db.String(64))
-    itemclass = db.Column(db.Integer, db.ForeignKey('checkclass.id'))
+    testname = db.Column(db.String(64))
+
+class testItem(db.Model):
+    __tablename__ = 'testitem'
+    id = db.Column(db.Integer, primary_key=True) #6位 3开头 自增
+    testitemname = db.Column(db.String(64))
+    blongclass = db.Column(db.Integer, db.FornignKey('testclass.id'))
 
 class InhospitalArea(db.Model):
     __tablename__ = 'inhospitalarea'
     id = db.Column(db.Integer, primary_key=True)
     areaname = db.Column(db.String(64))
 
-class BedInfo(db.Model):
-    __tablename__ = 'bedinfo'
-    id = db.Column(db.Integer, primary_key=True)
-    areaid = db.Column(db.Integer, db.ForeignKey('inhospitalarea.id'))
+#缺少住院病床
 
-class Price(db.Model):
-    __tablename__ = 'price'
-    optionid = db.Column(db.Integer) #为药品、检查、检验ID
-    price = db.Column(db.Float)
+
+
